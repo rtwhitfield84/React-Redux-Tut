8558b60ef57fc01e3ff912d947790ed885eac59f
Rico <richardthomaswhitfield@gmail.com>
2017-03-30 15:30:31 -0500

    passing data to components

--- dev/js/containers/user-list.js
+++ dev/js/containers/user-list.js
@@ -3,13 +3,18 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
 class UserList extends Component {
+
+	createListItems(){
+		return this.props.users.map((user) => {
+			return(
+				<li key={user.id}>{user.first} {user.last}</li>
+				);
+		});
+	}
 	render(){
 		return(
 			<ul>
-				<li>1</li>
-				<li>2</li>
-				<li>3</li>
-				<li>4</li>
+				{this.createListItems()}
 			</ul>
 			);
 	}
