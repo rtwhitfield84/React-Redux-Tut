61129413b83f492dc818e693a8fd67dfcb0ff774
Rico <richardthomaswhitfield@gmail.com>
2017-03-30 14:18:38 -0500

    store and reducer-users

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,8 +1,11 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from 'redux';
 
 
+
+const store = createStore();
 
 ReactDOM.render(
 <h1>Hello</h1>,
---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,33 @@
+export default function() {
+	return [
+		{
+			id: 1,
+			first: "Richard",
+			last: "Whitfield",
+			age: 32,
+			description: "Software Engineer",
+			thumbnail: "http://cdn77.sadanduseless.com/wp-content/uploads/2016/05/funny-animals12.jpg"
+		},	{
+			id: 2,
+			first: "Tom",
+			last: "Thumb",
+			age: 34,
+			description: "thumby fellow",
+			thumbnail: "https://s-media-cache-ak0.pinimg.com/originals/a0/9e/aa/a09eaa8e4b9edff7c6971217cee07304.jpg"
+		},	{
+			id: 3,
+			first: "Melanie",
+			last: "Macaroon",
+			age: 43,
+			description: "Cookie person",
+			thumbnail: "http://mercuryanimal.com/wp-content/uploads/2016/09/Sweet-Kitten-Animal-Image-1.jpg"
+		},	{
+			id: 4,
+			first: "Other",
+			last: "Guy",
+			age: 99,
+			description: "The other guy",
+			thumbnail: "http://www.hickerphoto.com/images/1024/endangered-animal-list_5406.jpg"
+		}
+	]
+}
