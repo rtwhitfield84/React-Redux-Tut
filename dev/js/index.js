3f2e3a30886f762d335e501992dc56ba327dc59c
Rico <richardthomaswhitfield@gmail.com>
2017-03-30 14:25:50 -0500

    reducers

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,10 +2,11 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
 import {createStore} from 'redux';
+import allReducers from './reducers';
 
 
 
-const store = createStore();
+const store = createStore(allReducers);
 
 ReactDOM.render(
 <h1>Hello</h1>,
